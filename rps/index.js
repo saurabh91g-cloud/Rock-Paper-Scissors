3ddc40ecfcafa885636499f61dd23543bdca7f13
Saurabh Kumar <saurabh91g@gmail.com>
2026-05-01 07:36:44 +0530

    Clean up comments in index.js
    Removed game rules and assumptions comments from the code.

--- rps/index.js
+++ rps/index.js
@@ -1,19 +1,6 @@
 /**
  * The Rock 🪨, Paper 🧻, or Scissors ✂️ Game
  * This game will be played between the computer and human. Once Human will select Rock, Paper, or Scissors and in the next turn the computer will select Rock, Paper, or Scissors.
- * The Winner will be selected based on these rules:
- * - Rock can Break Scissors => Rock Wins
- * - Paper can wrap Rock => paper Wins
- * - Scissors can cut Paper => Scissors Wins
- * Ask the input from user and randomly select a computer selection to continue the game.
- */
-
-// Assumptions:
-// 1. We have to prompt the user to get their inputs.
-// 2. The Computer's selection will be random.
-// 3. We have to compare user and computer's choice
-// 4. We need to announce the winner.
-// 5. After the Winner announcement, we may want to ask the user to play again or quit from the game.
 
 function rockPaperScissorsGame() {
     console.log("Getting Started With the Rock 🪨, Paper 🧻, or Scissors ✂️ Game");
